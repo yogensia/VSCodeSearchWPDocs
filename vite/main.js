f0e69ea9d8e71cc11169d5970715345e204e966b
Yog <yogensia@gmail.com>
2024-05-07 14:47:35 +0200

    Move SCSS preview pipeline to Vite and update themes

---
+++ vite/main.js
@@ -0,0 +1,5 @@
+import '../styles/sass/theme-one-dark.scss'
+// import '../styles/sass/theme-monokai.scss'
+import wpLogo from '../assets/wporg-logo.svg'
+
+document.querySelector('#main-header h1 img').src = wpLogo
